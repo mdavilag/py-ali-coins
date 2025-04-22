b73029ebd36af365d52f8b5b67455a7838c4bc4c
Matheus Davila <mat.97@hotmail.com>
2025-04-22 19:34:40 -0300

    Created permanent profile and code is working (to coin page)

--- script.py
+++ script.py
@@ -3,47 +3,42 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("mobileEmulation", {
     "deviceName": "iPhone X"
 })
+options.add_argument(r"user-data-dir=C:\\ChromeDriver\\Profiles\\profile")
 
-driver = webdriver.Chrome(options)
+service = Service(r"C:\\ChromeDriver\chromedriver.exe")
+
+
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("http://aliexpress.com")
-
+print("Aguardando")
+#input()
 wait = WebDriverWait(driver, 10)
 #input("Esperando..")
 login_box = wait.until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'comet-icon-account'))
+    EC.presence_of_element_located((By.CLASS_NAME, 'comet-icon-myaccount'))
 )
 #driver.find_element(By.XPATH,value='//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[3]/div/b/span[1]')
+time.sleep(2)
 login_box.click()
 print("Clicado no ícone")
-print("Procurando botão de outros")
-login_btn = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "scene-login-icon-more"))
+time.sleep(1)
+print("Procurando botão de moedas")
+coinMenu_btn = WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "comet-icon-coins"))
 )
 print("Achou botão")
 #driver.find_element(By.CLASS_NAME, value="my-account--signin--RiPQVPB")
-login_btn.click()
-
-login_input=wait.until(
-    EC.presence_of_element_located((By.CLASS_NAME,'cosmos-input'))
-)
-#driver.find_element(By.CLASS_NAME, '._1unVQ')
-login_input.click()
-login_input.send_keys("matdavila.97@gmail.com")
-
-login_input.send_keys(Keys.RETURN)
-login_pwd = wait.until(
-    EC.presence_of_element_located((By.ID, 'fm-login-password'))
-)
-login_pwd.click()
-login_pwd.send_keys("teste123")
-login_pwd.send_keys(Keys.RETURN)
+coinMenu_btn.click()
 input()
+
 
 
 input()
