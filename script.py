ba92e65fe55a19682eddb37ef7913786e8b8c21f
Matheus Davila <mat.97@hotmail.com>
2025-04-24 12:25:54 -0300

    Configured to get coins and added argument --headless

--- script.py
+++ script.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
 
@@ -11,6 +10,7 @@ options.add_experimental_option("mobileEmulation", {
     "deviceName": "iPhone X"
 })
 options.add_argument(r"user-data-dir=C:\\ChromeDriver\\Profiles\\profile")
+options.add_argument("--headless")
 
 service = Service(r"C:\\ChromeDriver\chromedriver.exe")
 
@@ -18,28 +18,38 @@ service = Service(r"C:\\ChromeDriver\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("http://aliexpress.com")
-print("Aguardando")
-#input()
+print("-- Entrando na pagina aliexpress")
 wait = WebDriverWait(driver, 10)
-#input("Esperando..")
+print("-- Procurando ícone da conta")
 login_box = wait.until(
     EC.presence_of_element_located((By.CLASS_NAME, 'comet-icon-myaccount'))
 )
 #driver.find_element(By.XPATH,value='//*[@id="_full_container_header_23_"]/div[2]/div/div[2]/div[3]/div/b/span[1]')
 time.sleep(2)
 login_box.click()
-print("Clicado no ícone")
+print("-- Ícone da conta clicado")
 time.sleep(1)
-print("Procurando botão de moedas")
+print("-- Procurando ícone de moedas")
 coinMenu_btn = WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.CLASS_NAME, "comet-icon-coins"))
 )
-print("Achou botão")
-#driver.find_element(By.CLASS_NAME, value="my-account--signin--RiPQVPB")
 coinMenu_btn.click()
-input()
+print("-- Ícone de moedas clicado - redirecionando para página de moedas")
 
-
+print("-- Procurando botão para resgatar moedas")
+try:
+    getCoins_btn = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'aecoin-unchecked-3rjes'))
+    )
+    getCoins_btn.click()
+    print("-- Moedas recolhidas com sucesso")
+except:
+    if(EC.presence_of_element_located((By.CLASS_NAME,'aecoin-checked-3eXDN'))):
+        print("-- Moedas já recolhidas hoje")
+    else:
+        print("-- Erro ao recolher moedas")
+    time.sleep(2)
+    print("-- Encerrando script - digite algo para finalizar")
 
 input()
 driver.quit()
